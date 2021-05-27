324a9b3ce9115c57b1cd798f4bd39a67fb28c5dd
S Nitesh Singh <niteshseram@gmail.com>
2021-05-27 19:11:12 +0530

    adding header footer component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,17 @@
+import { Container } from "react-bootstrap";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 function App() {
   return (
     <>
-      <h1>Welcome to Booksify</h1>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to Booksify</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   );
 }
