b08a012a177f760a2d183cb7f8b1f94559f3056f
S Nitesh Singh <niteshseram@gmail.com>
2021-05-30 12:08:22 +0530

    adding remove from cart functionality

--- frontend/src/actions/cart.js
+++ frontend/src/actions/cart.js
@@ -18,3 +18,12 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
 
   localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
 };
+
+export const removeFromCart = (id) => async (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id,
+  });
+
+  localStorage.setItem("cartItems", JSON.stringify(getState().cart.cartItems));
+};
--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -11,7 +11,7 @@ import {
   Card,
 } from "react-bootstrap";
 import Message from "../components/Message";
-import { addToCart } from "../actions/cart";
+import { addToCart, removeFromCart } from "../actions/cart";
 
 const CartPage = ({ match, location, history }) => {
   const productId = match.params.id;
@@ -31,7 +31,7 @@ const CartPage = ({ match, location, history }) => {
   }, [dispatch, productId, qty]);
 
   const removeFromCartHandler = (id) => {
-    console.log(id);
+    dispatch(removeFromCart(id));
   };
 
   const checkOutHandler = () => {
