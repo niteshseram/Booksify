0737b1528ac9d79d17fbc30e4e6d77ed33fcf2b4
S Nitesh Singh <niteshseram@gmail.com>
2021-05-30 20:02:19 +0530

    fixing some bug with the product controller

--- backend/controller/product.js
+++ backend/controller/product.js
@@ -13,6 +13,11 @@ export const getProducts = asyncHandler(async (req, res) => {
 // @route   GET /api/products/:id
 // @access  Public
 export const getProductById = asyncHandler(async (req, res) => {
-  const products = await Product.find();
-  res.json(products);
+  const product = await Product.findById(req.params.id);
+  if (product) {
+    res.json(product);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
 });
