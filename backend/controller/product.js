aa9df9fa7ef6c3d5cd73e58c03ff722767e0dff9
S Nitesh Singh <niteshseram@gmail.com>
2021-05-30 19:54:08 +0530

    fixing some comments

--- backend/controller/product.js
+++ backend/controller/product.js
@@ -1,11 +1,17 @@
 import asyncHandler from "express-async-handler";
 import Product from "./../models/product.js";
 
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  Public
 export const getProducts = asyncHandler(async (req, res) => {
   const products = await Product.find();
   res.json(products);
 });
 
+// @desc    Fetch a single product
+// @route   GET /api/products/:id
+// @access  Public
 export const getProductById = asyncHandler(async (req, res) => {
   const products = await Product.find();
   res.json(products);
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -2,14 +2,7 @@ import express from "express";
 const router = express.Router();
 import { getProductById, getProducts } from "./../controller/product.js";
 
-// @desc    Fetch all products
-// @route   GET /api/products
-// @access  Public
 router.route("/").get(getProducts);
-
-// @desc    Fetch a single product
-// @route   GET /api/products/:id
-// @access  Public
 router.route("/:id").get(getProductById);
 
 export default router;
