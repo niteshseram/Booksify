4f0759a337189d5a13c1348ade91bd772e6cb036
S Nitesh Singh <niteshseram@gmail.com>
2021-05-30 12:02:39 +0530

    designing the cart page

--- frontend/src/pages/CartPage.js
+++ frontend/src/pages/CartPage.js
@@ -20,13 +20,108 @@ const CartPage = ({ match, location, history }) => {
 
   const dispatch = useDispatch();
 
+  const cart = useSelector((state) => state.cart);
+
+  const { cartItems } = cart;
+
   useEffect(() => {
     if (productId) {
       dispatch(addToCart(productId, qty));
     }
   }, [dispatch, productId, qty]);
 
-  return <div>Cart</div>;
+  const removeFromCartHandler = (id) => {
+    console.log(id);
+  };
+
+  const checkOutHandler = () => {
+    history.push("/login?redirect=shipping");
+  };
+
+  return (
+    <Row>
+      <Col md={8}>
+        <h1>Shopping Cart</h1>
+        {cartItems.length === 0 ? (
+          <Message>
+            Your cart is empty <Link to="/">Go Back</Link>
+          </Message>
+        ) : (
+          <ListGroup variant="flush">
+            {cartItems.map((item) => (
+              <ListGroup.Item key={item.product}>
+                <Row>
+                  <Col md={2}>
+                    <Image src={item.image} alt={item.name} fluid rounded />
+                  </Col>
+                  <Col md={3}>
+                    <Link to={`/product/${item.product}`}>{item.name}</Link>
+                  </Col>
+                  <Col md={2}>Rs. {item.price}</Col>
+                  <Col md={2}>
+                    <Button
+                      className="minus"
+                      onClick={() =>
+                        dispatch(addToCart(item.product, item.qty - 1))
+                      }
+                      disabled={item.qty <= 1}
+                    >
+                      -
+                    </Button>
+                    <Form.Label className="qty-label">{item.qty}</Form.Label>
+                    <Button
+                      className="plus"
+                      onClick={() =>
+                        dispatch(addToCart(item.product, item.qty + 1))
+                      }
+                      disabled={item.qty === item.countInStock}
+                    >
+                      +
+                    </Button>
+                  </Col>
+                  <Col md={2}>
+                    <Button
+                      type="button"
+                      variant="light"
+                      onClick={() => removeFromCartHandler(item.product)}
+                    >
+                      <i className="fas fa-trash" />
+                    </Button>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
+        )}
+      </Col>
+      <Col md={4}>
+        <Card>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h2>
+                Subtotal ({cartItems.reduce((acc, item) => acc + item.qty, 0)})
+                items
+              </h2>
+              Rs.{" "}
+              {cartItems
+                .reduce((acc, item) => acc + item.qty * item.price, 0)
+                .toFixed(2)}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <Button
+                type="button"
+                className="d-grid col-12"
+                disabled={cartItems.length === 0}
+                onClick={checkOutHandler}
+              >
+                Proceed to Checkout
+              </Button>
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Col>
+    </Row>
+  );
 };
 
 export default CartPage;
