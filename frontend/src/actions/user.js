f714b0f2b19b0dd61dcb2b3c52d44f0a2f8d2a91
S Nitesh Singh <niteshseram@gmail.com>
2021-05-31 12:58:26 +0530

    conditional rendering in navbar based on user is logged in or not

--- frontend/src/actions/user.js
+++ frontend/src/actions/user.js
@@ -3,6 +3,7 @@ import {
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
 } from "./../constants/user";
 export const login = (email, password) => async (dispatch) => {
   try {
@@ -37,4 +38,9 @@ export const login = (email, password) => async (dispatch) => {
           : error.message,
     });
   }
+};
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem("userInfo");
+  dispatch({ type: USER_LOGOUT });
 };
