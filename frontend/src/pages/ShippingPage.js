4329308deaed247b431e60a39a72ec7c9a8f059e
S Nitesh Singh <niteshseram@gmail.com>
2021-05-31 17:52:59 +0530

    adding checkout steps component

--- frontend/src/pages/ShippingPage.js
+++ frontend/src/pages/ShippingPage.js
@@ -3,6 +3,7 @@ import { Form, Button } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import FormContainer from "./../components/FormContainer";
 import { saveShippingAddress } from "./../actions/cart";
+import CheckoutSteps from "../components/CheckoutSteps";
 
 const ShippingPage = ({ history }) => {
   const cart = useSelector((state) => state.cart);
@@ -30,6 +31,7 @@ const ShippingPage = ({ history }) => {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="address" className="mb-3">
