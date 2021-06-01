a2dc193935f63ce8af50114deab62dfe9ce50a43
S Nitesh Singh <niteshseram@gmail.com>
2021-06-01 10:02:55 +0530

    adding order details reducer and action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -9,7 +9,7 @@ import {
   userRegisterReducer,
   userUpdateProfileReducer,
 } from "./reducers/user";
-import { orderCreateReducer } from "./reducers/order";
+import { orderCreateReducer, orderDetailsReducer } from "./reducers/order";
 
 const reducer = combineReducers({
   productList: productListReducer,
@@ -20,6 +20,7 @@ const reducer = combineReducers({
   userDetails: userDetailsReducer,
   userUpdateProfile: userUpdateProfileReducer,
   orderCreate: orderCreateReducer,
+  orderDetails: orderDetailsReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem("cartItems")
