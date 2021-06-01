deee67a6d3da1482a152acb9f8fa2750f5c987af
S Nitesh Singh <niteshseram@gmail.com>
2021-06-01 09:51:53 +0530

    adding get order by id endpoint

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,8 +1,9 @@
 import express from "express";
 const router = express.Router();
-import { addOrderItems } from "../controller/order.js";
+import { addOrderItems, getOrderById } from "../controller/order.js";
 import { isSignedIn } from "./../middleware/auth.js";
 
 router.route("/").post(isSignedIn, addOrderItems);
+router.route("/:id").get(isSignedIn, getOrderById);
 
 export default router;
