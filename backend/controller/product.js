6b602fa833f76d2db8a6e475b8b9202858295ee6
S Nitesh Singh <niteshseram@gmail.com>
2021-06-03 14:07:29 +0530

    adding create and update product api end point

--- backend/controller/product.js
+++ backend/controller/product.js
@@ -35,3 +35,49 @@ export const deleteProduct = asyncHandler(async (req, res) => {
     throw new Error("Product not found");
   }
 });
+
+// @desc    Create a product
+// @route   POST /api/products
+// @access  Private/Admin
+export const createProduct = asyncHandler(async (req, res) => {
+  const product = new Product({
+    name: "Sample name",
+    price: 0,
+    user: req.user._id,
+    image: "/images/sample.jpg",
+    author: "Sample author",
+    genre: "Sample genre",
+    countInStock: 0,
+    numReviews: 0,
+    description: "Sample description",
+  });
+
+  const createdProduct = await product.save();
+  res.status(201).json(createdProduct);
+});
+
+// @desc    Update a product
+// @route   PUT /api/products/:id
+// @access  Private/Admin
+export const updateProduct = asyncHandler(async (req, res) => {
+  const { name, price, description, image, author, genre, countInStock } =
+    req.body;
+
+  const product = await Product.findById(req.params.id);
+
+  if (product) {
+    product.name = name;
+    product.price = price;
+    product.description = description;
+    product.image = image;
+    product.author = author;
+    product.genre = genre;
+    product.countInStock = countInStock;
+
+    const updatedProduct = await product.save();
+    res.json(updatedProduct);
+  } else {
+    res.status(404);
+    throw new Error("Product not found");
+  }
+});
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,16 +1,19 @@
 import express from "express";
 const router = express.Router();
 import {
+  createProduct,
   deleteProduct,
   getProductById,
   getProducts,
+  updateProduct,
 } from "./../controller/product.js";
 import { isAdmin, isSignedIn } from "./../middleware/auth.js";
 
-router.route("/").get(getProducts);
+router.route("/").get(getProducts).post(isSignedIn, isAdmin, createProduct);
 router
   .route("/:id")
   .get(getProductById)
-  .delete(isSignedIn, isAdmin, deleteProduct);
+  .delete(isSignedIn, isAdmin, deleteProduct)
+  .put(isSignedIn, isAdmin, updateProduct);
 
 export default router;
