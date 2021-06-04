d14a7dd3b900a72fd72c0701cc76476f8430abee
S Nitesh Singh <niteshseram@gmail.com>
2021-06-04 11:29:54 +0530

    adding image upload option in edit product page

--- frontend/src/pages/ProductEditPage.js
+++ frontend/src/pages/ProductEditPage.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Form, Button } from "react-bootstrap";
@@ -17,7 +18,8 @@ const ProductEditPage = ({ match, history }) => {
   const [author, setAuthor] = useState("");
   const [genre, setGenre] = useState("");
   const [countInStock, setCountInStock] = useState(0);
-  const [description, setDescription] = useState(0);
+  const [description, setDescription] = useState("");
+  const [uploading, setUploading] = useState(false);
 
   const dispatch = useDispatch();
 
@@ -49,6 +51,28 @@ const ProductEditPage = ({ match, history }) => {
       }
     }
   }, [product, dispatch, productId, history, successUpdate]);
+
+  const uploadFileHandler = async (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append("image", file);
+    setUploading(true);
+    try {
+      const config = {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      };
+
+      const { data } = await axios.post("/api/upload", formData, config);
+
+      setImage(data);
+      setUploading(false);
+    } catch (error) {
+      console.error(error);
+      setUploading(false);
+    }
+  };
 
   const submitHandler = (e) => {
     e.preventDefault();
@@ -107,6 +131,13 @@ const ProductEditPage = ({ match, history }) => {
                 value={image}
                 onChange={(e) => setImage(e.target.value)}
               ></Form.Control>
+              <Form.File
+                id="image-file"
+                label="Choose File"
+                custom
+                onChange={uploadFileHandler}
+              ></Form.File>
+              {uploading && <Loader />}
             </Form.Group>
             <Form.Group controlId="author" className="mb-3">
               <Form.Label>Author</Form.Label>
