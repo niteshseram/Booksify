87cdc521cef49ed87c95d45b02f6fde004ddfb82
S Nitesh Singh <niteshseram@gmail.com>
2021-06-04 12:12:45 +0530

    adding get all orders api end points

--- backend/controller/order.js
+++ backend/controller/order.js
@@ -80,3 +80,11 @@ export const getMyOrders = asyncHandler(async (req, res) => {
   const orders = await Order.find({ user: req.user._id });
   res.json(orders);
 });
+
+// @desc    Get all orders
+// @route   GET /api/orders/
+// @access  Private/Admin
+export const getOrders = asyncHandler(async (req, res) => {
+  const orders = await Order.find({}).populate("user", "id name");
+  res.json(orders);
+});
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -4,11 +4,15 @@ import {
   addOrderItems,
   getMyOrders,
   getOrderById,
+  getOrders,
   updateOrderToPaid,
 } from "../controller/order.js";
-import { isSignedIn } from "./../middleware/auth.js";
+import { isSignedIn, isAdmin } from "./../middleware/auth.js";
 
-router.route("/").post(isSignedIn, addOrderItems);
+router
+  .route("/")
+  .post(isSignedIn, addOrderItems)
+  .get(isSignedIn, isAdmin, getOrders);
 router.route("/myorders").get(isSignedIn, getMyOrders);
 router.route("/:id").get(isSignedIn, getOrderById);
 router.route("/:id/pay").put(isSignedIn, updateOrderToPaid);
