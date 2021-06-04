720730c605c4dc0c89e227c9c18c6124b001e943
S Nitesh Singh <niteshseram@gmail.com>
2021-06-04 12:42:48 +0530

    adding update order to deliver api endpoint

--- backend/controller/order.js
+++ backend/controller/order.js
@@ -73,6 +73,23 @@ export const updateOrderToPaid = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc    Update order to delivered
+// @route   PUT /api/orders/:id/deliver
+// @access  Private
+export const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isDelivered = true;
+    order.deliveredAt = Date.now();
+    const updatedOrder = await order.save();
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error("Order not found");
+  }
+});
+
 // @desc    Get logged in user orders
 // @route   GET /api/orders/myorders
 // @access  Private
--- backend/routes/order.js
+++ backend/routes/order.js
@@ -5,6 +5,7 @@ import {
   getMyOrders,
   getOrderById,
   getOrders,
+  updateOrderToDelivered,
   updateOrderToPaid,
 } from "../controller/order.js";
 import { isSignedIn, isAdmin } from "./../middleware/auth.js";
@@ -16,5 +17,6 @@ router
 router.route("/myorders").get(isSignedIn, getMyOrders);
 router.route("/:id").get(isSignedIn, getOrderById);
 router.route("/:id/pay").put(isSignedIn, updateOrderToPaid);
+router.route("/:id/deliver").put(isSignedIn, isAdmin, updateOrderToDelivered);
 
 export default router;
