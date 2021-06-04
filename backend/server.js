8d1bd8646ab89c863fd594b67fca759934f73196
S Nitesh Singh <niteshseram@gmail.com>
2021-06-04 11:29:36 +0530

    adding image uploading api end point

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
@@ -9,6 +10,7 @@ import productRoutes from "./routes/product.js";
 import userRoutes from "./routes/user.js";
 import orderRoutes from "./routes/order.js";
 import paymentRoutes from "./routes/payment.js";
+import uploadRoutes from "./routes/upload.js";
 
 dotenv.config();
 
@@ -27,7 +29,10 @@ app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/payment", paymentRoutes);
+app.use("/api/upload", uploadRoutes);
 
+const __dirname = path.resolve();
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 // middleware
 app.use(notFound);
 app.use(errorHandler);
