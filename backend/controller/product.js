6b5710e49068678d2f5135abf73c63ab25efc61e
S Nitesh Singh <niteshseram@gmail.com>
2021-06-05 12:50:14 +0530

    adding pagination query to the get products api end point

--- backend/controller/product.js
+++ backend/controller/product.js
@@ -5,6 +5,10 @@ import Product from "./../models/product.js";
 // @route   GET /api/products
 // @access  Public
 export const getProducts = asyncHandler(async (req, res) => {
+  const pageSize = 10;
+
+  const page = Number(req.query.pageNumber) || 1;
+
   const keyword = req.query.keyword
     ? {
         name: {
@@ -14,8 +18,12 @@ export const getProducts = asyncHandler(async (req, res) => {
       }
     : {};
 
-  const products = await Product.find({ ...keyword });
-  res.json(products);
+  const count = await Product.count({ ...keyword });
+
+  const products = await Product.find({ ...keyword })
+    .limit(pageSize)
+    .skip(pageSize * (page - 1));
+  res.json({ products, page, pages: Math.ceil(count / pageSize) });
 });
 
 // @desc    Fetch a single product
