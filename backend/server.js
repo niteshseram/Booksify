0e7051505a9a599c7a05ef42d56fa0508cbf6282
S Nitesh Singh <niteshseram@gmail.com>
2021-06-06 10:24:06 +0530

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -25,10 +25,6 @@ if (process.env.NODE_ENV === "development") {
 }
 
 //routes
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
-
 app.use("/api/products", productRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/orders", orderRoutes);
@@ -37,6 +33,18 @@ app.use("/api/upload", uploadRoutes);
 
 const __dirname = path.resolve();
 app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
+
 // middleware
 app.use(notFound);
 app.use(errorHandler);
