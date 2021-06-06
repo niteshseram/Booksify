b3dafd7a43526f79f59e884ef3faf19690bf90e7
S Nitesh Singh <niteshseram@gmail.com>
2021-06-06 14:57:44 +0530

    adding google analytics

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import ReactGA from "react-ga";
 import store from "./store";
 import "./bootstrap.min.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import RouteChangeTracker from "./components/RouteChangeTracker";
+
+ReactGA.initialize(process.env.TRACKING_ID);
+<RouteChangeTracker />;
 
 ReactDOM.render(
   <Provider store={store}>
